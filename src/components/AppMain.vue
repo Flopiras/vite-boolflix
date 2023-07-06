2dcea8ebc9993cf92dfa318575fd31eec1d4db3a
Flopiras <Flopiras@yahoo.it>
2023-07-06 16:52:46 +0200

    edit main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,21 +17,42 @@ export default {
 
 <template>
     <main>
-        <div class="container">
-            <h2 class="my-3">Film :</h2>
-            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
-                <div v-for="movie in movies" :key="movie.id" class="col">
-                    <FilmCard :film="movie" />
+        <section id="movies" class="py-3">
+            <div class="container">
+                <h2 class="pb-2">Film :</h2>
+                <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
+                    <div v-for="movie in movies" :key="movie.id" class="col">
+                        <FilmCard :film="movie" />
+                    </div>
                 </div>
             </div>
+        </section>
+        <section id="series" class="py-3">
+            <div class="container">
+                <h2 class="pb-2">Serie TV :</h2>
+                <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
+                    <div v-for="serie in series" :key="serie.id" class="col">
+                        <FilmCard :film="serie" />
 
-            <h2 class="my-3">Serie TV :</h2>
-            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
-                <div v-for="serie in series" :key="serie.id" class="col">
-                    <FilmCard :film="serie" />
-
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     </main>
 </template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/vars' as *;
+
+#movies {
+    background-color: $bg-movies;
+}
+
+.card {
+    border: 1px solid black;
+}
+
+#series {
+    background-color: $bg-series;
+}
+</style>
