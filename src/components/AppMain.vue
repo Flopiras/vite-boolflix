02662077de8dc9a78cd9e1f14e9b0ba579ddac43
Flopiras <Flopiras@yahoo.it>
2023-07-06 15:59:05 +0200

    edit AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,9 +16,22 @@ export default {
 </script>
 
 <template>
-    <h2>Film :</h2>
-    <FilmCard v-for="movie in movies" :key="movie.id" :film="movie" />
+    <main>
+        <div class="container">
+            <h2 class="my-3">Film :</h2>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
+                <div v-for="movie in movies" :key="movie.id" class="col">
+                    <FilmCard :film="movie" />
+                </div>
+            </div>
 
-    <h2>Serie TV :</h2>
-    <FilmCard v-for="serie in series" :key="serie.id" :film="serie" />
+            <h2 class="my-3">Serie TV :</h2>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4">
+                <div v-for="serie in series" :key="serie.id" class="col">
+                    <FilmCard :film="serie" />
+
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
