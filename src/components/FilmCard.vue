5461de2a8a55c2864cf87c00b75070c72076edaf
Flopiras <Flopiras@yahoo.it>
2023-07-06 11:51:49 +0200

    add flags

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -8,7 +8,18 @@ export default {
     props: {
         film: Object
     },
+    computed: {
+        hasFlag() {
+            const flags = ['it', 'en'];
+            return flags.includes(this.film.original_language)
+        },
 
+        imageFlagSrc() {
+            const url = new URL(`../assets/img/${this.film.original_language}.png`, import.meta.url);
+            console.log(url)
+            return url.href
+        }
+    }
 }
 
 </script>
@@ -17,6 +28,10 @@ export default {
     <ul>
         <li>Titolo: {{ film.title }}</li>
         <li>Titolo in lingua originale: {{ film.original_title }}</li>
+        <li>
+            <img v-if="hasFlag" :src="imageFlagSrc" :alt="film.original_language">
+            <span v-else>Lingua: {{ film.original_language }}</span>
+        </li>
         <li>Voto: {{ film.vote_average }}</li>
     </ul>
 </template>
