b9f5605538fb49e6d002cdf1153460e49523e860
Flopiras <Flopiras@yahoo.it>
2023-07-06 11:51:27 +0200

    add cards

---
+++ src/components/FilmCard.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        film: Object
+    },
+
+}
+
+</script>
+
+<template>
+    <ul>
+        <li>Titolo: {{ film.title }}</li>
+        <li>Titolo in lingua originale: {{ film.original_title }}</li>
+        <li>Voto: {{ film.vote_average }}</li>
+    </ul>
+</template>
